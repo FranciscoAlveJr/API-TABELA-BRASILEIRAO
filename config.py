5f6756333fbd3b82196db0e056d56551466e2587
Francisco Júnior <65497402+FranciscoAlveJr@users.noreply.github.com>
2023-12-20 10:53:41 -0300

    bug fix

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ SERVER = 'localhost'
 DB = 'tabela_brasileirao'
 
 # SQLALCHEMY_DATABASE_URI = f'mysql://{USERNAME}:{PASSWORD}@{SERVER}/{DB}'
-SQLALCHEMY_DATABASE_URI = 'postgres://tabela_brasileirao_user:YW2aR8v56V5UXeJuChBXAfeL2U1psQ5w@dpg-cm0q3q0l5elc73eeoe30-a.ohio-postgres.render.com/tabela_brasileirao'
+SQLALCHEMY_DATABASE_URI = 'postgresql://tabela_brasileirao_user:YW2aR8v56V5UXeJuChBXAfeL2U1psQ5w@dpg-cm0q3q0l5elc73eeoe30-a.ohio-postgres.render.com/tabela_brasileirao'
 # SQLALCHEMY_TRACK_MODIFICATIONS = True
 
-SECRET_KEY = 'aplicacao_flask'
+# SECRET_KEY = 'aplicacao_flask'
