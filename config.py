ff4e3841e45b5ab2be48997781b03d6f8c670843
Francisco Júnior <65497402+FranciscoAlveJr@users.noreply.github.com>
2024-04-09 15:16:08 -0300

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@ import os
 
 DEBUG = True
 
-USERNAME = 'root'
-PASSWORD = '123456'
-SERVER = 'localhost'
-DB = 'tabela_brasileirao'
+USERNAME = ''
+PASSWORD = ''
+SERVER = ''
+DB = ''
 
 # SQLALCHEMY_DATABASE_URI = f'mysql://{USERNAME}:{PASSWORD}@{SERVER}/{DB}'
-SQLALCHEMY_DATABASE_URI = 'postgresql://tabela_brasileirao_user:YW2aR8v56V5UXeJuChBXAfeL2U1psQ5w@dpg-cm0q3q0l5elc73eeoe30-a.ohio-postgres.render.com/tabela_brasileirao'
+SQLALCHEMY_DATABASE_URI = ''
 
 # SQLALCHEMY_TRACK_MODIFICATIONS = True
 
